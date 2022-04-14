a1f2667a5b58139c2a096bf7ed56cd43ada02e1e
arasopraza <aras.opraza@gmail.com>
2022-04-15 01:06:43 +0700

    Update model Menu, and add test for Menu

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,5 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_04_14_155138) do
-  create_table "menus", force: :cascade do |t|
-    t.string "name"
-    t.string "price"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
+ActiveRecord::Schema[7.0].define(version: 0) do
 end
