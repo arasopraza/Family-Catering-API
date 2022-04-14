b7f9a6e18dbf6191f8a44daea5f09f0487bbbb91
arasopraza <aras.opraza@gmail.com>
2022-04-15 01:09:27 +0700

    Fix error factory_bot, migrate db

--- db/schema.rb
+++ db/schema.rb
@@ -10,5 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 0) do
+ActiveRecord::Schema[7.0].define(version: 2022_04_14_180605) do
+  create_table "menus", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.float "price"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
 end
