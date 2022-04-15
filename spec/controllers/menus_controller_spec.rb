442f8bf4ef33b12618e5a2de372e4a8922fe86a8
arasopraza <aras.opraza@gmail.com>
2022-04-15 13:42:32 +0700

    Add spec Menus controller get #index

--- spec/controllers/menus_controller_spec.rb
+++ spec/controllers/menus_controller_spec.rb
@@ -1,4 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe MenusController do
+    describe 'GET #index' do
+        it "populates an array of all menus" do
+            ayam_geprek = FactoryBot.create(:menu, name: 'Ayam geprek')
+            ayam_penyet = FactoryBot.create(:menu, name: 'Ayam penyet')
+
+            get :index
+            expect(assigns(:menus)).to match_array([ayam_geprek, ayam_penyet])
+        end
+
+        it "renders the :index template" do
+        end
+    end
 end
